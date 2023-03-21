1493118027260d108ab3471c6e3a048010ff1618
Marcel Wuijtenburg <60228936+MarcelW1323@users.noreply.github.com>
2023-03-21 22:31:28 +0100

    update on testing of general functions
    Completed all "is-functions":
    def test_islist():
    def test_isdictionary():
    def test_isinteger():
    def test_isstring():
    def test_isfloat():
    def test_isboolean():
    def test_isdataframe():

--- clean_business_chart/tests/clean_business_chart/test_general_functions.py
+++ clean_business_chart/tests/clean_business_chart/test_general_functions.py
@@ -12,23 +12,72 @@ def test_islist():
     # Test with a list
     expected = True
     actual   = islist(['list'])
-    assert actual == expected, "islist(['list']) gives back "+str(actual)+" instead of "+str(exptected)
+    assert actual == expected, "islist(['list']) gives back "+str(actual)+" instead of "+str(expected)
     # Test with a string
     expected = False
     actual   = islist('not a list')
-    assert actual == expected, "islist('not a list') gives back "+str(actual)+" instead of "+str(exptected)
+    assert actual == expected, "islist('not a list') gives back "+str(actual)+" instead of "+str(expected)
 
 
 def test_isdictionary():
     # Test with a dictionary
     expected = True
     actual   = isdictionary({'dictionary':'yes'})
-    assert actual == expected, "isdictionary({'dictionary':'yes'}) gives back "+str(actual)+" instead of "+str(exptected)
+    assert actual == expected, "isdictionary({'dictionary':'yes'}) gives back "+str(actual)+" instead of "+str(expected)
     # Test with a string
     expected = False
     actual   = isdictionary('not a dictionary')
-    assert actual == expected, "isdictionary('not a dictionary') gives back "+str(actual)+" instead of "+str(exptected)
+    assert actual == expected, "isdictionary('not a dictionary') gives back "+str(actual)+" instead of "+str(expected)
 
+def test_isinteger():
+    # Test with an integer
+    expected = True
+    actual   = isinteger(3)
+    assert actual == expected, "isinteger(3) gives back "+str(actual)+" instead of "+str(expected)
+    # Test with a string
+    expected = False
+    actual   = isinteger('not an integer')
+    assert actual == expected, "isinteger('not an integer') gives back "+str(actual)+" instead of "+str(expected)
+
+def isstring():
+    # Test with a string
+    expected = True
+    actual   = isstring('this is a string')
+    assert actual == expected, "isstring('this is a string') gives back "+str(actual)+" instead of "+str(expected)
+    # Test with a string
+    expected = False
+    actual   = isstring(100)
+    assert actual == expected, "isstring(100) gives back "+str(actual)+" instead of "+str(expected)
+
+def isfloat(inputvariable):
+    # Test with a float
+    expected = True
+    actual   = isfloat(3.14)
+    assert actual == expected, "isfloat(3.14) gives back "+str(actual)+" instead of "+str(expected)
+    # Test with a string
+    expected = False
+    actual   = isfloat('not a float')
+    assert actual == expected, "isfloat('not a float') gives back "+str(actual)+" instead of "+str(expected)
+
+def isboolean(inputvariable):
+    # Test with a boolean
+    expected = True
+    actual   = isboolean(True)
+    assert actual == expected, "isboolean(True) gives back "+str(actual)+" instead of "+str(expected)
+    # Test with a string
+    expected = False
+    actual   = isboolean('not a boolean')
+    assert actual == expected, "isboolean('not a boolean') gives back "+str(actual)+" instead of "+str(expected)
+
+def isdataframe(inputvariable):
+    # Test with a DataFrame
+    expected = True
+    actual   = isdataframe(pd.DataFrame({'Column1': ['Value1', 'Value2']}))
+    assert actual == expected, "isdataframe(pd.DataFrame({'Column1': ['Value1', 'Value2']}) gives back "+str(actual)+" instead of "+str(expected)
+    # Test with a string
+    expected = False
+    actual   = isdataframe('not a DataFrame')
+    assert actual == expected, "isdataframe('not a DataFrame') gives back "+str(actual)+" instead of "+str(expected)
 
 def test_plot_line_accross_axes():
     # The function plot_line_accross_axes() draws a line in a figure object.
