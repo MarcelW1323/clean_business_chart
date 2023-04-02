99d41e8f14abdc4dbd45333fd0649f8856a8b496
Marcel Wuijtenburg <60228936+MarcelW1323@users.noreply.github.com>
2023-04-02 17:22:49 +0200

    Added new function for test
    Related with commit "DRY with dataframes"

--- clean_business_chart/tests/clean_business_chart/test_columnchartwithwaterfall.py
+++ clean_business_chart/tests/clean_business_chart/test_columnchartwithwaterfall.py
@@ -81,30 +81,63 @@ def test___convert_data_list_of_lists_to_dict():
         testvar = ColumnWithWaterfall(test=True)
         testvar._convert_data_list_of_lists_to_dict("This is a string")
 
-def test__convert_dataframe_to_list_of_lists():
-    # Test 1 - good dataframe to list_of_lists conversion
+def test__dataframe_search_for_headers():
+    # Test 1 - good dataframe, search for available headers and error when not found
     dataset = pd.DataFrame({'Year' : [2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022], 
                             'Month': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
                             'PY': [32, 38, 29, 35, 41, 37, 33, 38, 42, 44, 39, 31], 
                             'PL': [33, 35, 37, 40, 38, 36, 35, 40, 45.0328, 50.8, 45, 40], 
                             'AC': [35, 33, 41, 41, 37, 37, 0, 0, 0, 0, 0, 0], 
                             'FC': [0, 0, 0, 0, 0, 0, 38, 44, 46, 48, 44, 44]})
+    search_for_headers = ['Year', 'Month', 'PY', 'PL', 'AC', 'FC']
+    error_not_found    = True
     testvar  = ColumnWithWaterfall(test=True)
-    expected = [['Year', 'Month', 'PY', 'PL', 'AC', 'FC'], [2022.0, 1.0, 32.0, 33.0, 35.0, 0.0], [2022.0, 2.0, 38.0, 35.0, 33.0, 0.0], 
-                [2022.0, 3.0, 29.0, 37.0, 41.0, 0.0], [2022.0, 4.0, 35.0, 40.0, 41.0, 0.0], [2022.0, 5.0, 41.0, 38.0, 37.0, 0.0], 
-                [2022.0, 6.0, 37.0, 36.0, 37.0, 0.0], [2022.0, 7.0, 33.0, 35.0, 0.0, 38.0], [2022.0, 8.0, 38.0, 40.0, 0.0, 44.0], 
-                [2022.0, 9.0, 42.0, 45.0328, 0.0, 46.0], [2022.0, 10.0, 44.0, 50.8, 0.0, 48.0], [2022.0, 11.0, 39.0, 45.0, 0.0, 44.0], 
-                [2022.0, 12.0, 31.0, 40.0, 0.0, 44.0]]
-    actual   = testvar._convert_dataframe_to_list_of_lists(dataset)
-    message  = "Test 1 - ColumnWithWaterfall._convert_dataframe_to_list_of_lists returned {0} instead of {1}".format(actual, expected)
+    expected = search_for_headers
+    actual   = testvar._dataframe_search_for_headers(dataframe=dataset, search_for_headers=search_for_headers, error_not_found=error_not_found)
+    message  = "Test 1 - ColumnWithWaterfall._dataframe_search_for_headers returned {0} instead of {1}".format(actual, expected)
     assert actual == expected, message
 
-    # Test 2 - only string supported
+    # Test 2 - good dataframe, search for other headers also and no error when not found
+    dataset = pd.DataFrame({'Year' : [2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022], 
+                            'Month': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                            'PY': [32, 38, 29, 35, 41, 37, 33, 38, 42, 44, 39, 31], 
+                            'PL': [33, 35, 37, 40, 38, 36, 35, 40, 45.0328, 50.8, 45, 40], 
+                            'AC': [35, 33, 41, 41, 37, 37, 0, 0, 0, 0, 0, 0], 
+                            'FC': [0, 0, 0, 0, 0, 0, 38, 44, 46, 48, 44, 44]})
+    search_for_headers = ['Date', 'AC', 'Other']
+    error_not_found    = False
+    testvar  = ColumnWithWaterfall(test=True)
+    expected = ['AC']
+    actual   = testvar._dataframe_search_for_headers(dataframe=dataset, search_for_headers=search_for_headers, error_not_found=error_not_found)
+    message  = "Test 2 - ColumnWithWaterfall._dataframe_search_for_headers returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 3 - good dataframe, search for other headers also and error when not found
+    with pytest.raises(ValueError):
+        dataset = pd.DataFrame({'Year' : [2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022], 
+                                'Month': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                                'PY': [32, 38, 29, 35, 41, 37, 33, 38, 42, 44, 39, 31], 
+                                'PL': [33, 35, 37, 40, 38, 36, 35, 40, 45.0328, 50.8, 45, 40], 
+                                'AC': [35, 33, 41, 41, 37, 37, 0, 0, 0, 0, 0, 0], 
+                                'FC': [0, 0, 0, 0, 0, 0, 38, 44, 46, 48, 44, 44]})
+        search_for_headers = ['Date', 'AC', 'Other']
+        error_not_found    = True
+        testvar = ColumnWithWaterfall(test=True)
+        testvar._dataframe_search_for_headers(dataframe=dataset, search_for_headers=search_for_headers, error_not_found=error_not_found)
+
+    # Test 4 - only DataFrame supported as first parameter
     with pytest.raises(ValueError):
         testvar = ColumnWithWaterfall(test=True)
-        testvar._convert_dataframe_to_list_of_lists("This is a string")
+        testvar._dataframe_search_for_headers(dataframe="This is a string", search_for_headers=list())
+
+    # Test 5 - only list supported as second parameter
+    with pytest.raises(ValueError):
+        dataset = pd.DataFrame({'Year' : [2023]})
+        testvar = ColumnWithWaterfall(test=True)
+        testvar._dataframe_search_for_headers(dataframe=dataset, search_for_headers="This is a string")
+
     
-def test__data_frame_keep_only_relevant_columns():
+def test__dataframe_keep_only_relevant_columns():
     # Test 1 - good dataframe with extra columns
     dataset = pd.DataFrame({'Year' : [2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022], 
                             'Month': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
@@ -120,17 +153,18 @@ def test__data_frame_keep_only_relevant_columns():
                 'PL': {0: 33.0, 1: 35.0, 2: 37.0, 3: 40.0, 4: 38.0, 5: 36.0, 6: 35.0, 7: 40.0, 8: 45.0328, 9: 50.8, 10: 45.0, 11: 40.0}, 
                 'AC': {0: 35, 1: 33, 2: 41, 3: 41, 4: 37, 5: 37, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}, 
                 'FC': {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 38, 7: 44, 8: 46, 9: 48, 10: 44, 11: 44}}
-    actual   = testvar._data_frame_keep_only_relevant_columns(dataset)
+    actual   = testvar._dataframe_keep_only_relevant_columns(dataset)
     actual   = actual.to_dict()
-    message  = "Test 1 - ColumnWithWaterfall._data_frame_keep_only_relevant_columns returned {0} instead of {1}".format(actual, expected)
+    message  = "Test 1 - ColumnWithWaterfall._dataframe_keep_only_relevant_columns returned {0} instead of {1}".format(actual, expected)
     assert actual == expected, message
 
     # Test 2 - only string supported
     with pytest.raises(ValueError):
         testvar = ColumnWithWaterfall(test=True)
-        testvar._data_frame_keep_only_relevant_columns("This is a string")
+        testvar._dataframe_keep_only_relevant_columns("This is a string")
 
-def test__data_frame_aggregate():
+
+def test__dataframe_aggregate():
     # Test 1 - good dataframe with extra month entries
     dataset = pd.DataFrame({'Year' : [2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022, 2022], 
                             'Month': [1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
@@ -145,12 +179,12 @@ def test__data_frame_aggregate():
                 'PL': {0: 33.0, 1: 50.0, 2: 37.0, 3: 40.0, 4: 38.0, 5: 36.0, 6: 35.0, 7: 40.0, 8: 45.0328, 9: 50.8, 10: 45.0, 11: 40.0}, 
                 'AC': {0: 35, 1: 50, 2: 41, 3: 41, 4: 37, 5: 37, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}, 
                 'FC': {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 38, 7: 44, 8: 46, 9: 48, 10: 44, 11: 44}}
-    actual   = testvar._data_frame_aggregate(dataset)
+    actual   = testvar._dataframe_aggregate(dataset)
     actual   = actual.to_dict()
-    message  = "Test 1 - ColumnWithWaterfall._data_frame_aggregate returned {0} instead of {1}".format(actual, expected)
+    message  = "Test 1 - ColumnWithWaterfall._dataframe_aggregate returned {0} instead of {1}".format(actual, expected)
     assert actual == expected, message
 
     # Test 2 - only string supported
     with pytest.raises(ValueError):
         testvar = ColumnWithWaterfall(test=True)
-        testvar._data_frame_aggregate("This is a string")
+        testvar._dataframe_aggregate("This is a string")
