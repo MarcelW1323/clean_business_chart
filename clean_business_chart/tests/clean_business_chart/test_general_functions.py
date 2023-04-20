8082efcd3174307c29c39ab2bb658927624e6bb8
Marcel Wuijtenburg <marcelw1323@gmail.com>
2023-04-20 20:31:46 +0200

    added tests general functions

--- clean_business_chart/tests/clean_business_chart/test_general_functions.py
+++ clean_business_chart/tests/clean_business_chart/test_general_functions.py
@@ -136,4 +136,124 @@ def test_string_to_value():
     assert actual == expected, "string_to_value('1.867.16') gives back "+str(actual)+" instead of "+str(expected)
 
 
+def test_optimize_data():
+    # Test 1 - data-list to be rounded with 1 decimals
+    data     = [15, 3.56, 4.876, 45.932, 100]
+    expected = [15.0, 3.6, 4.9, 45.9, 100.0]
+    actual   = optimize_data(data=data, numerator=1, denominator=1, decimals=1)
+    message  = "Test 1 - optimize_data returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+    
+    # Test 2 - data-list to be rounded with 0 decimals
+    data     = [15, 3.56, 4.876, 45.932, 100]
+    expected = [15, 4, 5, 46, 100]
+    actual   = optimize_data(data=data, numerator=1, denominator=1, decimals=0)
+    message  = "Test 2 - optimize_data returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 3 - integer rounded with 1 decimals
+    data     = 16
+    expected = 16.0
+    actual   = optimize_data(data=data, numerator=1, denominator=1, decimals=1)
+    message  = "Test 3 - optimize_data returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 4 - float rounded with 0 decimals
+    data     = 16.37682
+    expected = 16
+    actual   = optimize_data(data=data, numerator=1, denominator=1, decimals=0)
+    message  = "Test 4 - optimize_data returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 5 - None
+    data     = None
+    expected = None
+    actual   = optimize_data(data=data, numerator=1, denominator=1, decimals=1)
+    message  = "Test 5 - optimize_data returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 6 - String
+    data     = "This is a string"
+    expected = "This is a string"
+    actual   = optimize_data(data=data, numerator=1, denominator=1, decimals=1)
+    message  = "Test 6 - optimize_data returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 7 - Optimizing with other numerator and denominator
+    data     = 123456789
+    expected = 370.4
+    actual   = optimize_data(data=data, numerator=3, denominator=1000000, decimals=1)
+    message  = "Test 7 - optimize_data returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 8 - Denominator = 0
+    with pytest.raises(ValueError):
+        data    = 3
+        optimize_data(data=data, numerator=3, denominator=0, decimals=1)
+
+    # Test 9 - Denominator = string
+    with pytest.raises(ValueError):
+        data    = 3
+        optimize_data(data=data, numerator=3, denominator="This is a string", decimals=1)
+
+    # Test 10 - Nominator = string
+    with pytest.raises(ValueError):
+        data    = 3
+        optimize_data(data=data, numerator="This is a string", denominator=1, decimals=1)
+
+    # Test 11 - Decimals = string
+    with pytest.raises(ValueError):
+        data    = 3
+        optimize_data(data=data, numerator=1, denominator=1, decimals="This is a string")
+
+    # Test 12 - Decimals = float
+    with pytest.raises(ValueError):
+        data    = 3
+        optimize_data(data=data, numerator=1, denominator=1, decimals=1.1)
+
+def test_formatstring():
+    # Test 1 - zero decimals
+    data     = 0
+    expected = '%0i'
+    actual   = formatstring(decimals=data)
+    message  = "Test 1 - formatstring returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 2 - one decimal
+    data     = 1
+    expected = '%0.1f'
+    actual   = formatstring(decimals=data)
+    message  = "Test 2 - formatstring returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 3 - two decimals
+    data     = 2
+    expected = '%0.2f'
+    actual   = formatstring(decimals=data)
+    message  = "Test 3 - formatstring returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 4 - three decimals
+    data     = 3
+    expected = '%0.3f'
+    actual   = formatstring(decimals=data)
+    message  = "Test 4 - formatstring returned {0} instead of {1}".format(actual, expected)
+    assert actual == expected, message
+
+    # Test 5 - Decimals = float
+    with pytest.raises(ValueError):
+        data    = 1.15
+        formatstring(decimals=data)
+
+    # Test 6 - Decimals = too high integer
+    with pytest.raises(ValueError):
+        data    = 4
+        formatstring(decimals=data)
+
+    # Test 7 - Decimals = string
+    with pytest.raises(ValueError):
+        data    = "This is a string"
+        formatstring(decimals=data)
+
+
 #### Need to add more test-functions for automatic testing
