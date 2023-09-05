1b004bc34cfba0acb4be87dc4d18f85ad3e7072d
Marcel Wuijtenburg <marcelw1323@gmail.com>
2023-09-05 23:10:45 +0200

    Added class TypeIntegerError

--- clean_business_chart/clean_business_chart/exceptions.py
+++ clean_business_chart/clean_business_chart/exceptions.py
@@ -11,6 +11,10 @@ class TypeDictionaryError(Exception):
     """Raised when the type is not a Dictionary"""
     pass
 
+class TypeIntegerError(Exception):
+    """Raised when the type is not a List"""
+    pass
+
 class TypeDataFrameError(Exception):
     """Raised when the type is not a pandas DataFrame"""
     pass
